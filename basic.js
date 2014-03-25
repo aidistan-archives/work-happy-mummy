edafbf986d4413159d790396025584125afa8e00
Aidi Stan <aidistan@live.com>
2014-03-26 03:35:38 +0800

    Refactor

--- basic.js
+++ basic.js
@@ -1,24 +1,14 @@
 current_dish = 1;
 
 $(function() {
-
+  // Hide loading layer
+  $("#loading").hide();
+  // Set views
+  $("#basket #content").css("margin", "0px " + 
+    $("#basket_rside").width() + "px 0px " + 
+    $("#basket_lside").width() + "px");
+  // Set callbacks
   $(window).resize(resize_page).trigger("resize");
-
-  /*
-   * Temp
-   */
-  $("div.basket div.content").css("margin", "0px " + 
-  	$("#basket_rside").width() + "px 0px " + 
-  	$("#basket_lside").width() + "px");
-  $("div.foreground").css("height", $(window).height()-100);
-
-  $("div.plate").click(function() {
-  	$("#dish_"+current_dish).addClass("hidden");
-  	current_dish = $(this).text();
-  	$("#dish_"+current_dish).removeClass("hidden");
-  });
-
-
 });
 
 function resize_page() {
@@ -26,7 +16,7 @@ function resize_page() {
 	 * Resize background_img
 	 */
   var win = $(window),
-      img = $("#background_img"),
+      img = $("#background img"),
       win_ratio = win.width() / win.height(),
       img_ratio = img.width() / img.height();
   // Fit center
