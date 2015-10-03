01977de77a5c7edfb24a73d529641d705a59f85c
Aidi Stan <aidistan@live.com>
2015-10-03 16:01:12 +0800

    Beatify ruby scripts

--- utility/generate_data.json.rb
+++ utility/generate_data.json.rb
@@ -1,25 +1,23 @@
 #!/usr/bin/env ruby
-# encoding: UTF-8
 require 'json'
 
-$if_pretty = true
-
 hash = {}
-File.open("data.txt").each do |line|
-  col = line.encode("UTF-8").chomp.split("\t")
+File.open('data.txt').each do |line|
+  col = line.encode('UTF-8').chomp.split("\t")
   hash[col[0]] = {}
-  hash[col[0]]['basket_img'] = 'images/basket/'+col[1]
+  hash[col[0]]['basket_img'] = 'images/basket/' + col[1]
   hash[col[0]]['basket_color'] = col[2]
-  hash[col[0]]['product_img'] = 'images/product/'+col[3]
+  hash[col[0]]['product_img'] = 'images/product/' + col[3]
   col[3] =~ /\d\d\d/
-  hash[col[0]]['desc'] = File.open("desc/#{$&}.html").read.gsub(/\s/, "")
-
+  hash[col[0]]['desc'] = File.open("desc/#{$&}.html").read.gsub(/\s/, '')
   hash[col[0]]['tags'] = {}
-  col[4].split(/\s*，\s*|\s*,\s*/).each { |tag|  hash[col[0]]['tags'][tag] = true }
+  col[4].split(/\s*，\s*|\s*,\s*/).each do |tag|
+    hash[col[0]]['tags'][tag] = true
+  end
 end
 
-fout = File.open("../data.json", 'w:utf-8')
-fout.puts $if_pretty ? JSON.pretty_generate(hash) : hash.to_json
+fout = File.open('../data.json', 'w:utf-8')
+fout.puts JSON.pretty_generate(hash)
 
-
-p hash.collect { |key, value| key+value['tags'].keys.join }.join
+# For youziku to generate the font CSS
+p hash.collect { |key, value| key + value['tags'].keys.join }.join
